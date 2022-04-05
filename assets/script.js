e8d80f48ded6f1a9978b3d853788ff5c984d330a
jennasaikaly <jen.saikaly@gmail.com>
2022-04-05 18:06:41 -0400

    js file up-to-date

--- assets/script.js
+++ assets/script.js
@@ -33,70 +33,77 @@ var getPlantInfo = function (plantInput) {
     var apiUrl = "https:openfarm.cc/api/v1/crops?filter=" + plantInput;
     console.log(apiUrl)
     fetch(apiUrl).then(function(response) {
+    // Clear data after search
+        plantContainerEl.textContent = "";
+        
         return response.json();
     })
     .then(function(data) {
-        debugger;
         console.log(data) 
 
-//     var imageId = plantResponse.data[0].relationships.pictures.data[0].id;
-
-//     var plantImage = "<img src='https://s3.amazonaws.com/openfarm-project/production/media/pictures/attachments/" + imageId + "'.' />"
-
-//   // Displaying plant results with an image from website
-//       var image = document.createElement("img")
-//       image.innerHTML = plantImage;
-//       plantContainerEl.innerHTML = plantName + plantImage; 
+      
+     
+   // Displaying plant results with an image from website
+      
+     var image = document.createElement("img");
+     image.src = data.data[0].attributes.main_image_path; 
+     image.setAttribute('width', '250px');
+     image.setAttribute('height', '200px');
+     image.innerHTML = image;
+     plantContainerEl.appendChild(image); 
 
     
     // Name pulled from OpenFarm
     var latinName = data.data[0].attributes.binomial_name
     var plantName = document.createElement("p"); 
-    plantName.innerHTML = " Binomial Name: " + latinName;
+    plantName.innerHTML = "<u><b>Binomial Name:</b></u> " + latinName;
     plantContainerEl.appendChild(plantName);
    
    //Description pulled from OpenFarm
    var description = data.data[0].attributes.description
    var plantDescription = document.createElement("p"); 
-   plantDescription.innerHTML = " Description: " + description;
+   plantDescription.innerHTML = "<u><b>Description:</b></u> " + description;
    plantContainerEl.appendChild(plantDescription);
 
    // Sun info pulled from OpenFarm
    var sun = data.data[0].attributes.sun_requirements;
    var plantSun = document.createElement("p")
-   plantSun.innerHTML = " Sun: " + sun;
+   plantSun.innerHTML = "<u><b>Sun Requirements:</b></u> " + sun;
    plantContainerEl.appendChild(plantSun);
 
    // Growth Info pulled from OpenFarm
    var growth = data.data[0].attributes.growing_degree_days;    
    var plantGrowth = document.createElement("p");
-   plantGrowth.innerHTML = " Growth: " + growth + " days";
+   plantGrowth.innerHTML = "<u><b>Growth:</b></u> " + growth + " days";
    plantContainerEl.appendChild(plantGrowth);
 
    // Sowing Steps pulled from OpenFarm
    var sow = data.data[0].attributes.sowing_method; 
    var plantSow = document.createElement("p");
-   plantSow.innerHTML = " Sowing Instructions: " + sow;
+   plantSow.innerHTML = "<u><b>Sowing Instructions:</b></u> " + sow;
    plantContainerEl.appendChild(plantSow);
 
    // Seed Spread pulled from OpenFarm
    var spread = data.data[0].attributes.spread;
    var plantSpread = document.createElement("p");
-   plantSpread.innerHTML = " Seed Spread: Plant seeds " + spread + "cm";
+   plantSpread.innerHTML = "<u><b>Seed Spread:</b></u> Plant seeds " + spread + "cm";
    plantContainerEl.appendChild(plantSpread);
 
    // Row Spacing pulled from OpenFarm
    var space = data.data[0].attributes.row_spacing;
    var plantSpace = document.createElement("p");
-   plantSpace.innerHTML = " Row Spacing: Make sure rows are " + space + "cm apart.";
+   plantSpace.innerHTML = "<u><b>Row Spacing:</b></u> Make sure rows are " + space + "cm apart.";
    plantContainerEl.appendChild(plantSpace);
 
    // Plant Height pulled from OpenFarm
    var height = data.data[0].attributes.height;
    var plantHeight = document.createElement("p");
-   plantHeight.innerHTML = " Plant Height: " + height + "cm tall.";
+   plantHeight.innerHTML = "<u><b>Plant Height:</b></u> " + height + "cm tall.";
    plantContainerEl.appendChild(plantHeight); 
+
+   
     })
+
 
 
 navigator.geolocation.getCurrentPosition(function(position) {
@@ -109,7 +116,8 @@ navigator.geolocation.getCurrentPosition(function(position) {
   console.log(data)
 });
   });
-}
 
+
+}
 
 searchButtonEl.addEventListener("click", formSubmitHandler);
