dfbf340c0aeb41a2fb49581484fd3191e7846b51
Dawit <dawit.tucker@live.com>
2022-04-07 02:12:11 -0400

    added icons

--- assets/script.js
+++ assets/script.js
@@ -154,7 +154,7 @@ navigator.geolocation.getCurrentPosition(function(position) {
     let lat = position.coords.latitude;
     let long = position.coords.longitude;
     console.log(lat, long)
-    const response = fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=9d88bd6fdf0dea57ceacfd94f52fe0b0&units=metric`, {
+    const response = fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&onecall?lat=${lat}&lon=${long}&appid=9d88bd6fdf0dea57ceacfd94f52fe0b0&units=metric`, {
   method: 'POST', // *GET, POST, PUT, DELETE, etc.
 }) .then(function(data){
   console.log(data)
